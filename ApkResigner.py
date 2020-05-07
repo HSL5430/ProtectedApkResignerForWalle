2c985fc49b6b1bacb109aec5b586c32402b4fcac
hesenlin <hesenlin@zuzuche.com>
2020-05-07 19:11:34 +0800

    支持传参channelsInputFilePath、channelsOutputFilePath

--- ApkResigner.py
+++ ApkResigner.py
@@ -18,15 +18,15 @@ import shutil
 
 #获取脚本文件的当前路径
 def curFileDir():
-     #获取脚本路径
-     path = sys.path[0]
-     #判断为脚本文件还是py2exe编译后的文件，
-     #如果是脚本文件，则返回的是脚本的目录，
-     #如果是编译后的文件，则返回的是编译后的文件路径
-     if os.path.isdir(path):
-         return path
-     elif os.path.isfile(path):
-         return os.path.dirname(path)
+  #获取脚本路径
+  path = sys.path[0]
+  #判断为脚本文件还是py2exe编译后的文件，
+  #如果是脚本文件，则返回的是脚本的目录，
+  #如果是编译后的文件，则返回的是编译后的文件路径
+  if os.path.isdir(path):
+    return path
+  elif os.path.isfile(path):
+    return os.path.dirname(path)
 
 #判断当前系统
 def isWindows():
@@ -38,10 +38,10 @@ def isWindows():
 
 #兼容不同系统的路径分隔符
 def getBackslash():
-	if(isWindows() == 1):
-		return "\\"
-	else:
-		return "/"
+  if(isWindows() == 1):
+    return "\\"
+  else:
+    return "/"
 
 # 重置Channels输出文件夹
 def resetChannelsDir():
@@ -57,14 +57,14 @@ def resetChannelsDir():
 
 #遍历当前目录下的apk文件
 def getApkFiles(dirname, ext='.apk'):
-    """获取目录下所有特定后缀的文件
-    @param dirname: str 目录的完整路径
-    @param ext: str 后缀名, 以点号开头
-    @return: list(str) 所有子文件名(不包含路径)组成的列表
-    """
-    return list(filter(
-      lambda filename: os.path.splitext(filename)[1] == ext,
-      os.listdir(dirname)))
+  """获取目录下所有特定后缀的文件
+  @param dirname: str 目录的完整路径
+  @param ext: str 后缀名, 以点号开头
+  @return: list(str) 所有子文件名(不包含路径)组成的列表
+  """
+  return list(filter(
+    lambda filename: os.path.splitext(filename)[1] == ext,
+    os.listdir(dirname)))
 
 # 生产渠道包
 def generateChannelApks(apkFilePath, channelFilePath):
@@ -72,28 +72,28 @@ def generateChannelApks(apkFilePath, channelFilePath):
   print ("**** channelFilePath:" + channelFilePath + " ****")
   zipalignedApkPath = apkFilePath[0 : -4] + "_aligned.apk"
   signedApkPath = zipalignedApkPath[0 : -4] + "_signed.apk"
-  
+
   #对齐
   zipalignShell = buildToolsPath + "zipalign -v 4 " + apkFilePath + " " + zipalignedApkPath
   os.system(zipalignShell)
-  
+
   #签名
   signShell = buildToolsPath + "apksigner sign --ks "+ keystorePath + " --ks-key-alias " + keyAlias + " --ks-pass pass:" + keystorePassword + " --key-pass pass:" + keyPassword + " --out " + signedApkPath + " " + zipalignedApkPath
   os.system(signShell)
   print(signShell)
-  
+
   #检查V2签名是否正确
   checkV2Shell = "java -jar " + checkAndroidV2SignaturePath + " " + signedApkPath
   os.system(checkV2Shell)
-  
+
   #写入渠道
   if len(config.extraChannelFilePath) > 0:
     writeChannelShell = "java -jar " + walleChannelWritterPath + " batch2 -f " + config.extraChannelFilePath + " " + signedApkPath + " " + channelsOutputFilePath
   else:
     writeChannelShell = "java -jar " + walleChannelWritterPath + " batch -f " + channelFilePath + " " + signedApkPath + " " + channelsOutputFilePath
-  
+
   os.system(writeChannelShell)
-  
+
   try:
     os.remove(zipalignedApkPath)
     os.remove(signedApkPath)
@@ -109,29 +109,50 @@ def getBuildToolsPath():
 #当前脚本文件所在目录
 parentDir = curFileDir()
 parentPath = parentDir + getBackslash()
+os.chdir(parentDir)
+
+def getChannelFilePath():
+  if len(sys.argv) > 2:
+    path = sys.argv[2] + getBackslash() + "channel.txt"
+  elif len(config.channelFilePath) > 0:
+    path = config.channelFilePath
+  else:
+    path = parentPath + "channel.txt"
+  return path
+
+def getChannelsInputFilePath():
+  if len(sys.argv) > 2:
+    path = sys.argv[2]
+  elif len(config.protectedSourceApkDirPath) > 0:
+    path = config.protectedSourceApkDirPath
+  else:
+    path = parentDir
+  return path + getBackslash()
+
+def getChannelsOutputFilePath():
+  if len(sys.argv) > 3:
+    path =  sys.argv[3]
+  elif len(config.channelsOutputFilePath) > 0:
+    path = config.channelsOutputFilePath
+  else:
+    path = parentPath + "channels"
+  return path
 
 #config
 libPath = parentPath + "lib" + getBackslash()
 buildToolsPath =  getBuildToolsPath() + getBackslash()
 checkAndroidV2SignaturePath = libPath + "CheckAndroidV2Signature.jar"
 walleChannelWritterPath = libPath + "walle-cli-all.jar"
-keystorePath = config.keystorePath
+
+keystorePath = os.path.abspath(config.keystorePath)
 keyAlias = config.keyAlias
 keystorePassword = config.keystorePassword
 keyPassword = config.keyPassword
-channelsOutputFilePath = parentPath + "channels"
-channelFilePath = parentPath +"channel.txt"
-protectedSourceApkPath = parentPath + config.protectedSourceApkName
 
-# 检查自定义路径，并作替换
-if len(config.protectedSourceApkDirPath) > 0:
-  protectedSourceApkPath = config.protectedSourceApkDirPath + getBackslash() + config.protectedSourceApkName
-
-if len(config.channelsOutputFilePath) > 0:
-  channelsOutputFilePath = config.channelsOutputFilePath
-
-if len(config.channelFilePath) > 0:
-  channelFilePath = config.channelFilePath
+channelFilePath = getChannelFilePath()
+channelsInputFilePath = getChannelsInputFilePath()
+protectedSourceApkPath = channelsInputFilePath + config.protectedSourceApkName
+channelsOutputFilePath = getChannelsOutputFilePath()
 
 # 创建Channels输出文件夹
 resetChannelsDir()
@@ -140,15 +161,15 @@ print ("\n**** =============================TASK BEGIN==========================
 print ("\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓   zipalign -->  apksigner -->  writeChannelShell   ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n")
 print ("\n**** =============================TASK BEGIN=================================== ****\n")
 
-apkFiles = getApkFiles(parentPath)
+apkFiles = getApkFiles(channelsInputFilePath)
 if len(apkFiles) > 0:
   for apkFile in apkFiles:
     # 获取apkFile对应的channel文件:channel_[apkName].txt
-    channelFile = parentPath + "channel_" + apkFile.replace('apk','txt')
+    channelFile = channelsInputFilePath + "channel_" + apkFile.replace('apk','txt')
     if os.path.isfile(channelFile):
       channelFilePath = channelFile
     # 渠道信息存在，批量打包对应的渠道
-    generateChannelApks(parentPath + apkFile, channelFilePath)
+    generateChannelApks(channelsInputFilePath + apkFile, channelFilePath)
 else:
   # 默认配置
   generateChannelApks(protectedSourceApkPath, channelFilePath)
